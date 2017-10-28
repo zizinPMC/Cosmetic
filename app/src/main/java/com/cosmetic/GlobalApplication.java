df903c3ea0747168db6c778a6b431ff44cf7e100
JiTichKim <kjhgood2101@naver.com>
2017-10-28 18:31:19 +0900

    change the kakao sdk

---
+++ app/src/main/java/com/cosmetic/GlobalApplication.java
@@ -0,0 +1,36 @@
+package com.cosmetic;
+
+import android.app.Activity;
+import android.app.Application;
+
+import com.kakao.auth.KakaoSDK;
+
+/**
+ * Created by gimjihyeon on 2017. 10. 12..
+ */
+
+public class GlobalApplication extends Application {
+
+    private static volatile GlobalApplication obj = null;
+    private static volatile Activity currentActivity = null;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        obj = this;
+        KakaoSDK.init(new KakaoSDKAdapter());
+    }
+
+    public static GlobalApplication getGlobalApplicationContext() {
+        return obj;
+    }
+
+    public static Activity getCurrentActivity() {
+        return currentActivity;
+    }
+
+    // Activity가 올라올때마다 Activity의 onCreate에서 호출해줘야한다.
+    public static void setCurrentActivity(Activity currentActivity) {
+        GlobalApplication.currentActivity = currentActivity;
+    }
+}
--- app/src/main/java/com/cosmetic/LoginActivity.java
+++ app/src/main/java/com/cosmetic/LoginActivity.java
@@ -3,9 +3,16 @@ package com.cosmetic;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.kakao.auth.ErrorCode;
 import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.LogoutResponseCallback;
+import com.kakao.usermgmt.callback.MeResponseCallback;
+import com.kakao.usermgmt.response.model.UserProfile;
 import com.kakao.util.exception.KakaoException;
 import com.kakao.util.helper.log.Logger;
 
@@ -14,53 +21,90 @@ import com.kakao.util.helper.log.Logger;
  */
 
 public class LoginActivity extends Activity {
-    private SessionCallback callback;      //콜백 선언
+    SessionCallback callback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_login);
 
-        callback = new SessionCallback();                  // 이 두개의 함수 중요함
+        //profileImg = (ImageView) findViewById(R.id.kakaoImg);
+        /**카카오톡 로그아웃 요청**/
+        //한번 로그인이 성공하면 세션 정보가 남아있어서 로그인창이 뜨지 않고 바로 onSuccess()메서드를 호출합니다.
+        //테스트 하시기 편하라고 매번 로그아웃 요청을 수행하도록 코드를 넣었습니다 ^^
+        UserManagement.requestLogout(new LogoutResponseCallback() {
+            @Override
+            public void onCompleteLogout() {
+                //로그아웃 성공 후
+
+            }
+        });
+
+        callback = new SessionCallback();
         Session.getCurrentSession().addCallback(callback);
 
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //간편로그인시 호출 ,없으면 간편로그인시 로그인 성공화면으로 넘어가지 않음
         if (Session.getCurrentSession().handleActivityResult(requestCode, resultCode, data)) {
             return;
         }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Session.getCurrentSession().removeCallback(callback);
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     private class SessionCallback implements ISessionCallback {
 
         @Override
         public void onSessionOpened() {
-            redirectSignupActivity();  // 세션 연결성공 시 redirectSignupActivity() 호출
-        }
 
+            UserManagement.requestMe(new MeResponseCallback() {
+
+                @Override
+                public void onFailure(ErrorResult errorResult) {
+                    String message = "failed to get user info. msg=" + errorResult;
+                    Logger.d(message);
+
+                    ErrorCode result = ErrorCode.valueOf(errorResult.getErrorCode());
+                    if (result == ErrorCode.CLIENT_ERROR_CODE) {
+                        finish();
+                    } else {
+                        //redirectMainActivity();
+                    }
+                }
+
+                @Override
+                public void onSessionClosed(ErrorResult errorResult) {
+                }
+
+                @Override
+                public void onNotSignedUp() {
+                }
+
+                @Override
+                public void onSuccess(UserProfile userProfile) {
+                    //로그인에 성공하면 로그인한 사용자의 일련번호, 닉네임, 이미지url등을 리턴합니다.
+                    //사용자 ID는 보안상의 문제로 제공하지 않고 일련번호는 제공합니다.
+                    Log.e("UserProfile=====>", userProfile.toString());
+
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+
+                    startActivity(intent);
+
+                    finish();
+
+                }
+            });
+
+
+
+        }
         @Override
         public void onSessionOpenFailed(KakaoException exception) {
-            if(exception != null) {
-                Logger.e(exception);
-            }
-            setContentView(R.layout.activity_login); // 세션 연결이 실패했을때
-        }                                            // 로그인화면을 다시 불러옴
-    }
-
-    protected void redirectSignupActivity() {       //세션 연결 성공 시
-        final Intent intent = new Intent(this, KakaoSignupActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-        startActivity(intent);
-        finish();
+            // 세션 연결이 실패했을때
+            // 어쩔때 실패되는지는 테스트를 안해보았음 ㅜㅜ
+        }
     }
 }
