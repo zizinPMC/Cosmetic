196c1533f8c7daa5b119d988a47d0c97c959e391
jeenstar <jeenstar11@gmail.com>
2017-11-02 03:06:08 +0900

    1.update board write,board fragment,home fragment, board write layout,fragment home 2.update manifest,,board write activity

--- app/src/main/java/com/cosmetic/HomeFragment.java
+++ app/src/main/java/com/cosmetic/HomeFragment.java
@@ -11,10 +11,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
-import com.cosmetic.db.BoardTipDB;
 import com.cosmetic.adapter.AutoScrollAdapter;
+import com.cosmetic.db.BoardTipDB;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -29,18 +28,16 @@ import cn.trinea.android.view.autoscrollviewpager.AutoScrollViewPager;
 
 public class HomeFragment extends Fragment {
 
-    @BindView(R.id.fab_store)
-    FloatingActionButton fab;
-    @BindView(R.id.home_autoViewPager)
-    AutoScrollViewPager autoViewPager;
-    @BindView(R.id.home_listview)
-    ListView listView;
-    @BindView(R.id.go_board)
-    Button btnGoBoard;
+    @BindView(R.id.fab_store) FloatingActionButton fab;
+    @BindView(R.id.home_autoViewPager) AutoScrollViewPager autoViewPager;
+    @BindView(R.id.home_listview) ListView listView;
+    @BindView(R.id.go_board) Button btnGoBoard;
 
-    ArrayList<HashMap<String, String>> tipList;//하단 리스트뷰 팁리스
+    ArrayList<HashMap<String,String>> tipList;//하단 리스트뷰 팁리스
 
-    public static HomeFragment newInstance() {
+    static View v;
+
+    public static HomeFragment newInstance(){
         HomeFragment fragment = new HomeFragment();
         return fragment;
     }
@@ -49,7 +46,7 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_home, container, false);
-        ButterKnife.bind(this, view);
+        ButterKnife.bind(this,view);
         return view;
     }
 
@@ -58,8 +55,8 @@ public class HomeFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         ArrayList<String> data = new ArrayList<>(); //이미지 url를 저장하는 arraylist
-        for (int i = 1; i <= 5; i++) {
-            data.add("http://zizin1318.cafe24.com/homeFlipImage/etude" + i + ".jpg");
+        for(int i=1;i<=5;i++){
+            data.add("http://zizin1318.cafe24.com/homeFlipImage/etude"+i+".jpg");
         }
         AutoScrollAdapter scrollAdapter = new AutoScrollAdapter(this.getContext(), data);
         autoViewPager.setAdapter(scrollAdapter); //Auto Viewpager에 Adapter 장착
@@ -68,25 +65,23 @@ public class HomeFragment extends Fragment {
 
         //하단 게시판 글 뷰
         tipList = new ArrayList<HashMap<String, String>>();
-        BoardTipDB.getData("http://zizin1318.cafe24.com/board/board_tip_read.php", tipList, this.getContext(), listView);
+        BoardTipDB.getData("http://zizin1318.cafe24.com/board/board_tip_read.php",tipList,this.getContext(),listView);
 
         //하단 게시판 더보기버튼 누를경우 게시판뷰로 이동
         btnGoBoard.setOnClickListener(listener);
 
     }
 
-    View.OnClickListener listener = new View.OnClickListener() {
+
+    View.OnClickListener listener = new View.OnClickListener(){
         @Override
         public void onClick(View v) {
-            switch (v.getId()) {
-                case R.id.go_board:
-                    Toast.makeText(getContext(), "button go board click", Toast.LENGTH_SHORT).show();
+            switch (v.getId()){
+                case R.id.go_board :
+                    Fragment fragment = BoardFragment.newInstance();
                     FragmentManager fm = getFragmentManager();
                     FragmentTransaction ft = fm.beginTransaction();
-                    Fragment fragment = BoardFragment.newInstance();
-                    ft.add(R.id.layout_fragment_home, fragment);
-                    ft.commit();
-
+                    ft.replace(R.id.layout_fragment_home, fragment).addToBackStack(null).commit();
                     break;
                 case R.id.fab_store:
 
--- app/src/main/java/com/cosmetic/board/BoardWriteActivity.java
+++ app/src/main/java/com/cosmetic/board/BoardWriteActivity.java
@@ -1,9 +1,11 @@
 package com.cosmetic.board;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,10 +14,14 @@ import android.widget.Spinner;
 import com.cosmetic.R;
 import com.cosmetic.db.BoardWriteDB;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
+import static java.sql.Types.NULL;
 
 /**
  * Created by yujeen on 2017. 9. 21..
@@ -23,21 +29,11 @@ import butterknife.ButterKnife;
 
 public class BoardWriteActivity extends AppCompatActivity {
 
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
-    /*@BindView(R.id.txt_nickname)
-    TextView nickname;*/
-
-    //@BindView(R.id.cbw_et_category) EditText editCategory;
-    @BindView(R.id.cbw_sp_category)
-    Spinner spinCategory;
+    @BindView(R.id.cbw_sp_category) Spinner spinCategory;
     //@BindView(R.id.cbw_et_writer) EditText editWriter;
-    @BindView(R.id.cbw_et_title)
-    EditText editTitle;
-    @BindView(R.id.cbw_et_contents)
-    EditText editContents;
-    @BindView(R.id.cbw_bt_submit)
-    Button btnSubmit;
+    @BindView(R.id.cbw_et_title) EditText editTitle;
+    @BindView(R.id.cbw_et_contents) EditText editContents;
+    @BindView(R.id.cbw_bt_submit) Button btnSubmit;
 
     private BoardWriteDB dbManager;
     private ArrayList<String> results;
@@ -50,7 +46,7 @@ public class BoardWriteActivity extends AppCompatActivity {
         setContentView(R.layout.activity_board_write);
         ButterKnife.bind(this);
 
-        setSupportActionBar(toolbar);
+        //setSupportActionBar(toolbar);
         //nickname.setText(UserInfo.userName+"  님");
 
         ArrayAdapter adapter = new ArrayAdapter(
@@ -66,10 +62,10 @@ public class BoardWriteActivity extends AppCompatActivity {
         dbManager = new BoardWriteDB();
 
         results = new ArrayList<String>();
-        //btnSubmit.setOnClickListener(listener);
+        btnSubmit.setOnClickListener(listener);
 
     }
-/*
+
     View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -77,17 +73,35 @@ public class BoardWriteActivity extends AppCompatActivity {
                 //String sCategory = editCategory.getText().toString();
                 String sCategory = spinCategory.getSelectedItem().toString();
                 //String sWriter = editWriter.getText().toString();
-                String sWriter = UserInfo.userName;
+                //String sWriter = UserInfo.userName;
+                String sWriter = "한뉴";
                 String sTitle = editTitle.getText().toString();
                 String sContents = editContents.getText().toString();
                 SimpleDateFormat sdfNow = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
                 Date date = new Date(System.currentTimeMillis());
                 String sDate =sdfNow.format(date).toString();
                 dbManager.comBoardDBManager(sCategory,sWriter, sTitle,sContents,sDate,"",NULL,0);
-                //(String boardCategory, String boardWriter, String boardTitle, String boardContent, String boardDate, String boardMdate, int boardHits, int boardGood) {
-                //Intent intent = new Intent(ComBoardWriteActivity.this,ComBoardTipActivity.class);
-                //startActivity(intent);
+                finish();
             }
         }
-    };*/
+    };
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+        AlertDialog.Builder d = new AlertDialog.Builder(this);
+        d.setMessage("글 작성을 종료하시겠습니까?");
+        d.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        d.setNegativeButton("아니요", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        d.show();
+
+    }
 }
